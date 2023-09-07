e5e8e04a708e1b6a89d4bc529e68d69d5879036d
QLittleMonster <411051941@qq.com>
2023-09-07 15:41:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 // import App from './App'
 // import App from './Components/Home'
 // import App from './组件传值父传子/App'
-// import App from './组件传值子传父/App'
-import App from './组件传值tab栏切换/App'
+import App from './组件传值子传父/App'
+// import App from './组件传值tab栏切换/App'
 
 
 
